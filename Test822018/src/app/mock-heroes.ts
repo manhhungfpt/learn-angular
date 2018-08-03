655449ccd7655a5cec4350991e5bef0167f381ec
DESKTOP-GJNK608\Manh Hung <hungnmgch15283@fpt.edu.vn>
2018-08-03 14:15:29 +0700

    Angular Demo 1

---
+++ Test822018/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero'
+
+export const HEROES:Hero[]=[
+{id:11, name:'Mr. Nice'},
+{id:12, name:'Narco'},
+{id:13, name:'Mr. Nice 2'},
+{id:14, name:'Mr. Nice 3'},
+{id:15, name:'Mr. Nice 4'},
+{id:16, name:'Mr. Nice 5'},
+{id:17, name:'Mr. Nice 6'},
+{id:18, name:'Mr. Nice 7'},
+{id:19, name:'Mr. Nice 8'},
+{id:20, name:'Mr. Nice 9'},
+];
